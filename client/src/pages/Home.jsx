60514ad7cf59c7f43382447bc6d6e5dca0ab8645
Renan Andrade <77841497+Laggh@users.noreply.github.com>
2025-03-24 22:13:23 -0300

    fixed key prop error

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,14 +2,15 @@ import ProductCard from "../components/ProductCard"
 
 const products = [
   { id: 1, name: "Jogo legal", shortDesc: "Um jogo muito legal", price: 20.30, image: "image.png"  },
+  { id: 2, name: "Jogo legal", shortDesc: "Um jogo muito legal", price: 20.30, image: "image.png"  },
 ];
 
 function Home(){
     return (
       <div className="container mt-5">
         <div className="row">
-          {products.map((product)=> (
-            <ProductCard product={product}/>
+          {products.map((product, index)=> (
+            <ProductCard key={index} product={product}/>
           ))}
         </div>
       </div>
